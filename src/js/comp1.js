6fc62b2c230f5adb9c9e5b7fdd78184f6d516c5b
santosh <santosh.ece06@gmail.com>
2019-01-28 18:36:54 +0530

    basic webpack

---
+++ src/js/comp1.js
@@ -0,0 +1,18 @@
+export default class Component1 {
+
+    addition(){
+        return "ADD";
+    }
+
+    subtraction() {
+        return "SUBTRACTION";
+    }
+
+    multiplication(){
+        return "MULTIPLICATION";
+    }
+
+    division(){
+        return "DIVISION";
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import Component1  from './comp1';
+
+let comp1 = new Component1();
+console.log(comp1.addition());
+console.log(comp1.subtraction());
+console.log(comp1.multiplication());
+console.log(comp1.division());
