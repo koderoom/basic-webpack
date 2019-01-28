1b2902978d238933f2fcb5250800bbc0609ccb4f
santosh <santosh.ece06@gmail.com>
2019-01-28 20:41:43 +0530

    html update.

---
+++ src/js/comp2.js
@@ -0,0 +1,23 @@
+
+export default class Component2 {
+
+    create() {
+        console.log("create");
+        return "Create";
+    }
+
+    read() {
+        console.log("read");
+        return "read";
+    }
+
+    update() {
+        console.log("update");
+        return "update";
+    }
+
+    delete() {
+        console.log("delete");
+        return "delete";
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // custom
 import Component1  from './comp1';
+import Component2 from './comp2';
 
 let comp1 = new Component1();
 console.log(comp1.addition());
 console.log(comp1.subtraction());
 console.log(comp1.multiplication());
 console.log(comp1.division());
+
+let comp2 = new Component2();
+console.log(comp2.create());
