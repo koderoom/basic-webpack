bbc7ec60630ad4ab9e60e04997a8850145e6e4d4
santosh <santosh.ece06@gmail.com>
2019-01-28 23:43:14 +0530

    webpack.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap';
+
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// custom js
+import clickme from './js/comp3';
+import {opr1, opr2} from './js/comp3';
+
+
+// custom css
+import './css/style.css'
+
+
+export {clickme, opr1, opr2};
--- src/js/comp3.js
+++ src/js/comp3.js
@@ -1,4 +1,19 @@
+
+let moment = require('moment');
+
 export default function clickme(){
     console.log("Click Me!!!");
 }
 
+
+let opr1 = function(){
+    console.log("Operation 1");
+}
+
+let opr2 = function(){
+    console.log(moment().format("YYYY-MM-DD HH:mm:ss"));
+    console.log("Operation 1");
+}
+
+
+export {opr1, opr2}
