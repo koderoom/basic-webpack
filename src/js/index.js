9570c55b143acd3ca5600cde09cf78c76f1b29db
santosh <santosh.ece06@gmail.com>
2019-01-28 23:23:49 +0530

    basic web pack.

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,8 @@ import 'bootstrap';
 // css
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 // custom
-import Component1  from './comp1';
-import Component2 from './comp2';
-
 import clickme from './comp3';
 
-window.handleClickMe = function(){
-    clickme();
-};
+
+export {clickme};
